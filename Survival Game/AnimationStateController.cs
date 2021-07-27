3838fd2011b5c1ff98226f6a54b52759ddbe66af
damiralic <84499542+damiralic@users.noreply.github.com>
2021-07-27 23:06:45 +0200

    Moja prva igra, napravljena u C#
    Ovu igru sam radio u unityju u C# jeziku, igra nije nikada objavljena ali ovo su neka od skripti koje sam napisao

---
+++ Survival Game/AnimationStateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (!Input.GetKey("w"))
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if(Input.GetKey("w") && Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", true);
+        }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
---
+++ Survival Game/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public static Tree instance;
+    public GameObject logPrefab;
+    public GameObject logPrefab2;
+    public int health;
+    public Animator anim;
+
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Shake()
+    {
+        anim.SetTrigger("Shake");
+    }
+    public void TakeDamage()
+    {
+        Shake();
+        health--;
+        if (health <= 0)
+        {
+            Instantiate(logPrefab, transform.position, transform.rotation);
+            Instantiate(logPrefab2, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
